901051061fd1913234badc7a5b7e2da1e9bb5988
amucinom <aldomucinomartinez@gmail.com>
2015-11-10 23:49:37 -0700

    Added all assignments to their respective folders

---
+++ Assignment1/server.js
@@ -0,0 +1,51 @@
+var http = require('http');
+var querystring = require('querystring');
+var url = require('url');
+
+var callback = function(req, res) {
+
+	var query = url.parse(req.url).query;
+	var route = req.url.split("?")[0];
+	var params = querystring.parse(query);
+
+	console.log(req.url);
+	console.log(route);
+	console.log(params);
+
+	if (route === "/getFibonacci") {
+
+		var num = parseFloat(params.n);
+
+		var fib = function(n) {
+			if (n <= 1) {
+				return n;
+			} else {
+				return fib(n - 1) + fib(n - 2);
+			}
+		};
+		// var r = parseFloat(params.a) + parseFloat(params.b);
+		res.writeHead(200, {
+			'Content-Type': 'text/plain'
+		});
+
+		for (var i = 0; i < num; i++) {
+			var result = fib(i);
+			var resultToHTML = fib(i) + '\n';
+			console.log(result.toString());
+			// document.getElementsByTagName('body').innerHTML = result;
+			res.write(resultToHTML.toString());
+		}
+		res.end();
+		// res.end(r.toString());
+	} else {
+		res.writeHead(200, {
+			'Content-Type': 'text/plain'
+		});
+		res.end("unidentified route");
+	}
+};
+
+var server = http.createServer(callback);
+server.listen(3942, "127.0.0.1");
+
+console.log('Server running at http://localhost:3942/');
---
+++ README.md
@@ -0,0 +1,15 @@
+# Makeup for all Assignments - AME 394 Advanced JS Programming
+
+## Instructions:
+### Assignment 1:
+ - $ node server.js
+ - In browser, type in following url, with any number as n:
+     + localhost:3942/getFibonacci?n=9
+
+### Assignment 2 & 3:
+ - Test any sort of expression
+
+### Assignment 4:
+ - Add a variety of nodes
+ - Change their location by dragging
+ - In the console, look at *linkedList.nodes* before and after dragging them to their new location
